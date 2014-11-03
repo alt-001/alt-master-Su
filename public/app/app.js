623390f20f878bd242d69e76a884ee373d91f018
liangsu1210 <su@c14creative.com>
2014-11-03 22:23:55 +0000

    account-page
    finished page:love-product,love-brand,order,profile
    
    combined previous jade structure to minimal version,deleted some unused
    image assets

--- public/app/app.js
+++ public/app/app.js
@@ -4,7 +4,19 @@ alt
 .config(function($routeProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$routeProvider
+
 	.when('/', { templateUrl: '/pages/index' })
+
+	/* ==========================================================================
+	Account page
+	========================================================================== */
+	.when('/account/love-products', { templateUrl: '/pages/account/love-products' })
+	.when('/account/love-brand', { templateUrl: '/pages/account/love-brand' })
+	.when('/account/order', { templateUrl: '/pages/account/order' })
+	.when('/account/profile', { templateUrl: '/pages/account/profile' })
+	/* ==========================================================================
+	Brand page
+	========================================================================== */
 	.when('/brand/products', { templateUrl: '/pages/brand/brand-products-all' })
 	.when('/brand/products/all', { templateUrl: '/pages/brand/brand-products-all' })
 	.when('/brand/products/featured', { templateUrl: '/pages/brand/brand-products-featured' })
